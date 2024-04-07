a2fbf090f91c5424076cecc8d65b0ef85815a6c5
Mykola Podopryhora <m.podopryhora@gmail.com>
2024-04-07 16:50:54 +0300

    formatting

--- js/task-3.js
+++ js/task-3.js
@@ -5,7 +5,7 @@ function getElementWidth(content, padding, border) {
     return contentWidth + paddingWidth + borderWidth;
 }
 
-console.log("Task 3:")
+console.log("Task 3:");
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px"));
 console.log(getElementWidth("200px", "0px", "0px"));
